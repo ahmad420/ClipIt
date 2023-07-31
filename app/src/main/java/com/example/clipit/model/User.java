eb0a4864f8b47c5e748b496e77077b72c425fee9
Ahmad <ahmdaf420@gmail.com>
2023-07-31 21:46:42 +0300

    user class added and ,some logos to drawables

---
+++ app/src/main/java/com/example/clipit/model/User.java
@@ -0,0 +1,31 @@
+package com.example.clipit.model;
+
+public class User {
+    private String email;
+    private String name;
+
+    // Required public no-argument constructor
+    public User() {
+    }
+
+    public User(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
